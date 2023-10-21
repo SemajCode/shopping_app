ee6b0bde8b2eea272e946628c5f9aa4c81138c19
SemajCode <mfonjames23@gmail.com>
2023-10-21 22:05:47 +0100

    changes added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,12 +5,46 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Hello World',
-        ),
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color.fromRGBO(225, 225, 225, 1),
+      ),
+      borderRadius: BorderRadius.horizontal(
+        left: Radius.circular(50),
       ),
     );
+
+    return const Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Shoes\nCollection',
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(width: 8),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: border,
+                    enabledBorder: border,
+                    focusedBorder: border,
+                    hintText: 'Search',
+                    prefixIcon: Icon(Icons.search),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          c
+        ],
+      ),
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Shop App',
       theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 254, 254, 2),
+        ),
         fontFamily: 'Noto-Sans',
+        inputDecorationTheme: const InputDecorationTheme(
+            hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+            prefixIconColor: Color.fromRGBO(119, 119, 119, 1)),
       ),
       home: const HomePage(),
     );
