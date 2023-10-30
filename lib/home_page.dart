dc5c2f7d8a8cb54abd77f35aeabe1e45bb375673
SemajCode <mfonjames23@gmail.com>
2023-10-30 11:56:22 +0100

    Using the chip widget

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    const List<String> filters = ['All' 'Addidas', 'Nike', 'Bata'];
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = ['All', 'Addidas', 'Nike', 'Beta'];
+  late String selectedFilter;
+
+  @override
+  void initState() {
+    selectedFilter = filters[0];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     const border = OutlineInputBorder(
       borderSide: BorderSide(
         color: Color.fromRGBO(225, 225, 225, 1),
@@ -45,9 +57,42 @@ class HomePage extends StatelessWidget {
             ],
           ),
           const SizedBox(height: 20),
-          ListView.builder(
-            itemCount: filters.length,
-            itemBuilder: (BuildContext context, index) {},
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: filters.length,
+              itemBuilder: (BuildContext context, index) {
+                final filter = filters[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedFilter = filter;
+                      });
+                    },
+                    child: Chip(
+                      side: const BorderSide(
+                        color: Color.fromRGBO(237, 242, 247, 1),
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      label: Text(filter),
+                      labelStyle: const TextStyle(fontSize: 15),
+                      backgroundColor: selectedFilter == filter
+                          ? const Color.fromARGB(255, 254, 254, 2)
+                          : const Color.fromRGBO(245, 247, 249, 1),
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 20,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
           )
         ],
       ),
