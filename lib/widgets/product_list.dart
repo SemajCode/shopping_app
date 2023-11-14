eaa959c40a35ec94e25ce1f110ba8734788faa20
SemajCode <mfonjames23@gmail.com>
2023-11-14 21:34:28 +0100

    changes added

--- lib/widgets/product_list.dart
+++ lib/widgets/product_list.dart
@@ -92,42 +92,66 @@ class _ProductListState extends State<ProductList> {
               },
             ),
           ),
-
           Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-              ),
-              itemBuilder: (context, index) {},
-            ),
+            child: size.width > 650
+                ? GridView.builder(
+                    itemCount: products.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      childAspectRatio: 2,
+                      crossAxisCount: 2,
+                    ),
+                    itemBuilder: (context, index) {
+                      final product = products[index];
+                      var color = const Color.fromRGBO(245, 247, 249, 1);
+                      if (index.isEven) {
+                        color = const Color.fromRGBO(216, 240, 253, 1);
+                      }
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  ProductDetails(product: product),
+                            ),
+                          );
+                        },
+                        child: ProductCard(
+                          title: product['title'] as String,
+                          price: product['price'] as double,
+                          image: product['imageUrl'] as String,
+                          backgroundColor: color,
+                        ),
+                      );
+                    },
+                  )
+                : ListView.builder(
+                    itemCount: products.length,
+                    itemBuilder: (context, index) {
+                      final product = products[index];
+                      var color = const Color.fromRGBO(245, 247, 249, 1);
+                      if (index.isEven) {
+                        color = const Color.fromRGBO(216, 240, 253, 1);
+                      }
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  ProductDetails(product: product),
+                            ),
+                          );
+                        },
+                        child: ProductCard(
+                          title: product['title'] as String,
+                          price: product['price'] as double,
+                          image: product['imageUrl'] as String,
+                          backgroundColor: color,
+                        ),
+                      );
+                    },
+                  ),
           )
-          // Expanded(
-          //   child: ListView.builder(
-          //     itemCount: products.length,
-          //     itemBuilder: (context, index) {
-          //       final product = products[index];
-          //       var color = const Color.fromRGBO(245, 247, 249, 1);
-          //       if (index.isEven) {
-          //         color = const Color.fromRGBO(216, 240, 253, 1);
-          //       }
-          //       return GestureDetector(
-          //         onTap: () {
-          //           Navigator.of(context).push(
-          //             MaterialPageRoute(
-          //               builder: (context) => ProductDetails(product: product),
-          //             ),
-          //           );
-          //         },
-          //         child: ProductCard(
-          //           title: product['title'] as String,
-          //           price: product['price'] as double,
-          //           image: product['imageUrl'] as String,
-          //           backgroundColor: color,
-          //         ),
-          //       );
-          //     },
-          //   ),
-          // )
         ],
       ),
     );
